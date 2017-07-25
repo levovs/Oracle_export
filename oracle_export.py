35fff315892990d9f71655bf9c122d6eb27a6d15
LEv <lev@samaradom.ru>
2017-07-25 15:29:26 +1000

    first commit

---
+++ oracle_export.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+import cx_Oracle
+dir="Export"
+if os.path.exists(dir):
+   shutil.rmtree(dir)
+if not os.path.exists(dir):
+  os.makedirs(dir)
+os.environ["NLS_LANG"] = ".UTF8"
+con = cx_Oracle.connect('python1/12345678@localhost/xe')
+cur = con.cursor()
+cur1 = con.cursor()
+objectTypeChars=['PACKAGE','VIEW','PROCEDURE','FUNCTION','TYPE','JOB','TABLE']
+for ii in (objectTypeChars):
+   dir_res=dir+"\\"+ii                                                                 
+   if not os.path.exists(dir_res):                                                                  
+     os.makedirs(dir_res)                                                                           
+                                                                                                    
+                                                                                                    
+   cur.execute('select OBJECT_NAME from user_objects where OBJECT_TYPE= :1',(ii,))     
+   for i in cur:                                                                                    
+       print(i[0])                                                                                  
+       Val = str(i[0])                                                                              
+       cur1.execute('select dbms_metadata.get_ddl(:1, :2) from dual',(ii,i[0],))       
+       rec1 = cur1.fetchone()                                                                       
+       with open(dir_res+"\\"+Val+".sql", "w") as f:                                                
+         f.write(rec1[0].read())                                                                    
+
+cur.close()
+cur1.close()
+con.close() 
